7da71ce8b7849ddd6ee6735bbf8199293d5164d9
sb62dev <sb62dev@gmail.com>
2023-10-21 16:43:12 +0530

    Component Added

--- myapp/src/app/page.tsx
+++ myapp/src/app/page.tsx
@@ -1,8 +1,10 @@
+import Header from "@components/Header";
 import Image from "next/image";
 
 export default function Home() {
     return (
         <>
+        <Header />
             <div className="hero_area"> 
                 <header className="header_section">
                 <div className="container-fluid">
